532cd91dcae4f034429aa8a702976a2fde40315d
Ioan Labici <ioan.labici@amdaris.com>
2022-12-22 11:39:05 +0200

    Added react and linked vite in welcome.blade.php

---
+++ resources/js/app.jsx
@@ -0,0 +1,12 @@
+import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './components/Home.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+)
---
+++ resources/js/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return ( 
+        <div className='react-in-laravel'>
+            Hello from React.
+        </div>
+     )
+}
+ 
+export default Home;
